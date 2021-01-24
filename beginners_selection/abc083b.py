b8761dcb28ad9bc5a1db7cc9241b968d0d2532cc
yucharotaro <yucharotaro@gmail.com>
2021-01-24 15:18:07 +0900

    Fix loop in abc083b.py.

--- beginners_selection/abc083b.py
+++ beginners_selection/abc083b.py
@@ -2,6 +2,6 @@ N, A, B = map(int, input().split())
 
 print(
     sum([
-        i for i in range(N + 1)
+        i for i in range(1, N + 1)
         if (sum(map(int, str(i))) >= A and (sum(map(int, str(i))) <= B))
     ]))
