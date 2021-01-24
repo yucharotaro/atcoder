fff614cea9d3119ea8f6d333ac4c05c72c3d13b1
yucharotaro <yucharotaro@gmail.com>
2021-01-24 15:23:13 +0900

    Fix handling of list in abc088b.py.

--- beginners_selection/abc088b.py
+++ beginners_selection/abc088b.py
@@ -2,6 +2,4 @@ N = int(input())
 a_list = map(int, input().split())
 sorted_a_list = sorted(a_list)[::-1]
 
-print(
-    sum(n for i, n in enumerate(sorted_a_list) if i % 2 == 0) -
-    sum(n for i, n in enumerate(sorted_a_list) if i % 2 != 0))
+print(sum(sorted_a_list[::2]) - sum(sorted_a_list[1::2]))
