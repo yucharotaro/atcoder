e71a546102e48879a98ce5d1dc96d8dabd9f4bdb
yucharotaro <yucharotaro@gmail.com>
2021-06-03 00:08:22 +0900

    Add c2.py in abc_contest_136.

--- abc_contest_136/ac/c2.py
+++ abc_contest_136/ac/c2.py
@@ -2,11 +2,10 @@ N = int(input())
 H = list(map(int, input().split()))
 
 for i in range(N - 1, 0, -1):
-    print(H[i - 1], H[i])
-    if H[i - 1] + 1 > H[i]:
+    if H[i - 1] - 1 > H[i]:
         print("No")
         exit()
-    else:
+    elif H[i - 1] - 1 == H[i]:
         H[i - 1] -= 1
 
 print("Yes")
