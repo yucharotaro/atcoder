fbb6d461d901821b30e20915b94927ef1db2c5e5
yucharotaro <yucharotaro@gmail.com>
2021-06-08 23:46:11 +0900

    Add b2.py in abc_contest_108.

--- abc_contest_108/b2.py
+++ abc_contest_108/b2.py
@@ -1,5 +1,14 @@
 x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = 0, 0, 0, 0
 
-r = int(((x1 - x2)**2 + (y1 - y2)**2)**0.5)
-print(r)
+# ベクトルを用いて考える。
+# x1からx2に向かうベクトル座標を(dx, dy)とする。
+dx = x2 - x1
+dy = y2 - y1
+
+# x2からx3、x3からx4に向かうベクトルの座標を求める。
+for i in range(2):
+    dx, dy = -dy, dx
+    x2 += dx
+    y2 += dy
+    print(x2, y2, end=" ")
+print()
